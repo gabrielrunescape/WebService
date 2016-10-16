7683abe2bacdd2d0b616e0c1e21e3f8fffc5907f
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-15 23:33:08 -0300

    Definição dos métodos de requição

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.end('GET users');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.end('GET users/' + req.params.id);
+});
+
+router.post('/', function(req, res, next) {
+  res.end('POST users');
+});
+
+router.put('/', function(req, res, next) {
+  res.end('PUT users');
+});
+
+router.delete('/:id', function(req, res, next) {
+  res.end('DELETE users/' + req.params.id);
 });
 
 module.exports = router;
