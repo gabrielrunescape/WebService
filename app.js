3716823d3765193f6348f7383654c83a78bb72fc
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-16 01:18:20 -0200

    Finalização dos métodos de requisição com MongoDB

--- app.js
+++ app.js
@@ -22,38 +22,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define as rotas da aplicação
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// Porta da aplicação
+app.set('port', (process.env.PORT || 3000));
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(app.get('port'), function(){
+	console.log('Servidor iniciado na porta ' + app.get('port'));
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,109 @@
 var express = require('express');
 var router  = express.Router();
 
+var db_string = 'mongodb://127.0.0.1/api';
+var mongoose  = require('mongoose').connect(db_string);
+
+var database  = mongoose.connection;
+var Usuario;
+
+database.on('error', console.error.bind(console, 'Erro ao conectar ao banco!'));
+
+database.once('open', function() {
+    var userSchema = mongoose.Schema({
+        login: String,
+        senha: String,
+        email: String,
+        Data_Criacao: Date
+    });
+
+    Usuario = mongoose.model('Usuario', userSchema);
+});
+
 router.get('/', function(req, res, next) {
-  res.end('GET users');
+    Usuario.find({}, function(err, usr) {
+        if (err) {
+            res.json({error: 'Não foi possível encontrar os usuários!'})
+        } else {
+            res.json(usr);
+        }
+    });
 });
 
 router.get('/:id', function(req, res, next) {
-  res.end('GET users/' + req.params.id);
+    var id = req.params.id;
+
+    Usuario.findById(id, function(err, usr) {
+        if (err) {
+            res.json({error: 'Não foi possível encontrar o usuário!'})
+        } else {
+            res.json(usr);
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
-  res.end('POST users');
+    var login = req.params.login;
+    var senha = req.params.senha;
+    var email = req.params.email;
+
+    new Usuario({
+        login: login,
+        senha: senha,
+        email: email,
+        Data_Criacao: new Date()
+    }).save(function(err, usr) {
+        if (err) {
+            res.json({error: 'Não foi possível salvar o usuário!'})
+        } else {
+            res.json(usr);
+        }
+    })
 });
 
 router.put('/', function(req, res, next) {
-  res.end('PUT users');
+    var id    = req.params.id;
+    var login = req.params.login;
+    var senha = req.params.senha;
+    var email = req.params.email;
+
+    Usuario.findById(id, function(err, usr){
+        if (login) {
+            usr.login = login;
+        }
+
+        if (senha) {
+            usr.senha = senha;
+        }
+
+        if (email) {
+            usr.email = email;
+        }
+
+        usr.save(function(err, usr) {
+            if (err) {
+                res.json({error: 'Não foi possível modificar o usuário!'})
+            } else {
+                res.json(usr);
+            }
+        });
+    });
 });
 
 router.delete('/:id', function(req, res, next) {
-  res.end('DELETE users/' + req.params.id);
+    var id = req.params.id;
+
+    Usuario.findById(id, function(err, usr) {
+        if (err) {
+            res.json({error: 'Não foi possível encontrar o usuário!'})
+        } else {
+            usr.remove(function(err) {
+                if (!err) {
+                    res.json({response: 'Usuário excluído com sucesso!'});
+                }
+            })
+        }
+    });
 });
 
 module.exports = router;
