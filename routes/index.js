70bddd563c9a576ddb03bb57c159784a5fe8ac14
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-26 22:49:41 -0200

    Inserção de página 404 (not fount)

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WebServive - API' });
 });
 
+router.get('*', function(req, res, next) {
+    if (res.status(404)) {
+        res.render('404', { title: 'Não encontrado - API' });
+    }
+});
+
 module.exports = router;
